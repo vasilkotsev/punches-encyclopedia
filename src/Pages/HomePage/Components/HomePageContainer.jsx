18f1717577b2ed07ed7d5a23eacc9e23d8c62930
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-10-15 15:03:05 +0300

    adds some refactoring for async functions and style fixes

--- src/Pages/HomePage/Components/HomePageContainer.jsx
+++ src/Pages/HomePage/Components/HomePageContainer.jsx
@@ -6,36 +6,33 @@ class HomePageContainer extends Component {
   state = {
     punches: [],
     error: null,
-    searchField: "",
+    searchField: ""
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({ searchField: event.target.value });
   };
 
-  componentDidMount() {
-    const asyncFuncRequest = async () => {
-      try {
-        const punchesResponse = await fetch(
-          "https://punches-encyclopedia-f1b45-default-rtdb.firebaseio.com/punches.json"
-        );
+  async componentDidMount() {
+    try {
+      const punchesResponse = await fetch(
+        "https://punches-encyclopedia-f1b45-default-rtdb.firebaseio.com/punches.json"
+      );
 
-        const punches = await punchesResponse.json();
+      const punches = await punchesResponse.json();
 
-        this.setState({
-          punches,
-        });
-      } catch (error) {
-        this.setState({ error: error.message });
-      }
-    };
-    asyncFuncRequest();
+      this.setState({
+        punches
+      });
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
   }
 
   render() {
     const { punches, error, searchField } = this.state;
 
-    const filteredPunches = punches.filter((punch) =>
+    const filteredPunches = punches.filter(punch =>
       punch.name.toLowerCase().includes(searchField.toLowerCase())
     );
     if (error) return <p style={{ textAlign: "center" }}>{error}</p>;
--- src/Pages/PunchPreviewPage/Components/PunchPreviewContainer.jsx
+++ src/Pages/PunchPreviewPage/Components/PunchPreviewContainer.jsx
@@ -5,44 +5,41 @@ import PunchPreview from "./Shared/PunchPreview";
 class PunchPreviewContainer extends Component {
   state = {
     punch: {},
-    error: null,
+    error: null
   };
 
-  mapToViewModel = (punch) => {
+  mapToViewModel = punch => {
     return {
       _id: punch._id,
       name: punch.name,
       type: punch.type,
       description: punch.description,
-      contents: punch.contents,
+      contents: punch.contents
     };
   };
 
-  componentDidMount() {
-    const asyncFuncRequest = async () => {
-      try {
-        const punchesResponse = await fetch(
-          "https://punches-encyclopedia-f1b45-default-rtdb.firebaseio.com/punches.json"
-        );
-        const punches = await punchesResponse.json();
-        const punchId = this.props.match.params.id;
+  async componentDidMount() {
+    try {
+      const punchesResponse = await fetch(
+        "https://punches-encyclopedia-f1b45-default-rtdb.firebaseio.com/punches.json"
+      );
+      const punches = await punchesResponse.json();
+      const punchId = this.props.match.params.id;
 
-        const punch = punches.find((p) => {
-          return p._id === punchId;
-        });
+      const punch = punches.find(p => {
+        return p._id === punchId;
+      });
 
-        if (!punch) return this.props.history.replace("/not-found");
+      if (!punch) return this.props.history.replace("/not-found");
 
-        this.setState({
-          punch: this.mapToViewModel(punch),
-        });
-      } catch (error) {
-        this.setState({
-          error: error.message,
-        });
-      }
-    };
-    asyncFuncRequest();
+      this.setState({
+        punch: this.mapToViewModel(punch)
+      });
+    } catch (error) {
+      this.setState({
+        error: error.message
+      });
+    }
   }
 
   render() {
